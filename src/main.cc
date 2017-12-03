a206c6d7ae4b0c76a3bb1cef5119f4f7df7da218
Brendan Forster <github@brendanforster.com>
2017-12-03 23:01:07 +1100

    refactor to tidy up the control flow

--- src/main.cc
+++ src/main.cc
@@ -62,117 +62,107 @@ NAN_METHOD(ReadValues) {
   if (openKey == ERROR_FILE_NOT_FOUND) {
     // the key does not exist, just return an empty array for now
     info.GetReturnValue().Set(New<v8::Array>(0));
-    return;
   }
+  else if (openKey == ERROR_SUCCESS)
+  {
+    TCHAR achClass[MAX_PATH] = TEXT("");  // buffer for class name
+    DWORD cchClassName = MAX_PATH;        // size of class string
+    DWORD cValues;                        // number of values for key
+    DWORD cchMaxValue;                    // longest value name
+    DWORD cbMaxValueData;                 // longest value data
 
+    TCHAR achValue[MAX_VALUE_NAME];
+    DWORD cchValue = MAX_VALUE_NAME;
 
-  if (openKey != ERROR_SUCCESS) {
+    auto retCode = RegQueryInfoKey(
+      hCurrentKey,
+      achClass,
+      &cchClassName,
+      NULL, // reserved
+      NULL, // can ignore subkey values
+      NULL,
+      NULL,
+      &cValues,
+      &cchMaxValue,
+      &cbMaxValueData,
+      NULL, // can ignore these values
+      NULL);
+
+    if (retCode != ERROR_SUCCESS) {
+      char* errorMessage = NULL;
+      sprintf(errorMessage, "RegQueryInfoKey failed - exit code: '%d'", retCode);
+      Nan::ThrowTypeError(errorMessage);
+    } else {
+      auto results = New<v8::Array>(cValues);
+      info.GetReturnValue().Set(results);
+
+      DWORD i;
+      for (i = 0, retCode = ERROR_SUCCESS; i < cValues; i++)
+      {
+        cchValue = MAX_VALUE_NAME;
+        achValue[0] = '\0';
+
+        DWORD lpType;
+        DWORD cbData = cbMaxValueData;
+        auto buffer = new byte[cbMaxValueData];
+
+        retCode = RegEnumValue(
+          hCurrentKey,
+          i,
+          achValue,
+          &cchValue,
+          NULL,
+          &lpType,
+          buffer,
+          &cbData);
+
+        if (retCode == ERROR_SUCCESS)
+        {
+          if (lpType == REG_SZ)
+          {
+            auto text = (char *)buffer;
+            auto obj = CreateEntry(achValue, "REG_SZ", text);
+            Nan::Set(results, i, obj);
+          }
+          else if (lpType == REG_EXPAND_SZ)
+          {
+            auto text = (char *)buffer;
+            auto obj = CreateEntry(achValue, "REG_EXPAND_SZ", text);
+            Nan::Set(results, i, obj);
+          }
+          else if (lpType == REG_DWORD)
+          {
+            // NOTE:
+            // at this point the value in buffer looks like this: '\x124242'
+            // i haven't figured out an easy way to parse this, so I'm going to make
+            // a second call because I know I can get the value out in the correct
+            // format in this way and avoid messing with strings
+            unsigned long size = 1024;
+
+            LONG nError = RegQueryValueEx(hCurrentKey, achValue, NULL, &lpType, (LPBYTE)&cbData, &size);
+            if (ERROR_SUCCESS == nError)
+            {
+              Nan::Set(results, i, CreateEntry(achValue, "REG_DWORD", cbData));
+            }
+          }
+        }
+        else if (retCode != ERROR_NO_MORE_ITEMS)
+        {
+          char* errorMessage = NULL;
+          sprintf(errorMessage, "RegEnumValue returned an error code: '%d'", retCode);
+          Nan::ThrowTypeError(errorMessage);
+        }
+      }
+
+      RegCloseKey(hCurrentKey);
+    }
+  }
+  else
+  {
     char* errorMessage = NULL;
     sprintf(errorMessage, "RegOpenKeyEx failed - exit code: '%d'", openKey);
     Nan::ThrowTypeError(errorMessage);
-    return;
   }
-
-  TCHAR achClass[MAX_PATH] = TEXT("");  // buffer for class name
-  DWORD cchClassName = MAX_PATH;        // size of class string
-  DWORD cSubKeys = 0;                   // number of subkeys
-  DWORD cbMaxSubKey;                    // longest subkey size
-  DWORD cchMaxClass;                    // longest class string
-  DWORD cValues;                        // number of values for key
-  DWORD cchMaxValue;                    // longest value name
-  DWORD cbMaxValueData;                 // longest value data
-
-  TCHAR achValue[MAX_VALUE_NAME];
-  DWORD cchValue = MAX_VALUE_NAME;
-
-  auto retCode = RegQueryInfoKey(
-    hCurrentKey,
-    achClass,
-    &cchClassName,
-    NULL, // reserved
-    &cSubKeys,
-    &cbMaxSubKey,
-    &cchMaxClass,
-    &cValues,
-    &cchMaxValue,
-    &cbMaxValueData,
-    NULL, // can ignore these values
-    NULL);
-
-  if (retCode != ERROR_SUCCESS) {
-    char* errorMessage = NULL;
-    sprintf(errorMessage, "RegQueryInfoKey failed - exit code: '%d'", retCode);
-    Nan::ThrowTypeError(errorMessage);
-    return;
-  }
-
-  if (cValues == 0) {
-    info.GetReturnValue().Set(New<v8::Array>(0));
-    return;
-  }
-
-  Local<Array> a = New<v8::Array>(cValues);
-  info.GetReturnValue().Set(a);
-
-  DWORD i;
-  for (i = 0, retCode = ERROR_SUCCESS; i<cValues; i++)
-  {
-    cchValue = MAX_VALUE_NAME;
-    achValue[0] = '\0';
-
-    DWORD lpType;
-    DWORD cbData = cbMaxValueData;
-    auto buffer = new byte[cbMaxValueData];
-
-    retCode = RegEnumValue(
-      hCurrentKey,
-      i,
-      achValue,
-      &cchValue,
-      NULL,
-      &lpType,
-      buffer,
-      &cbData);
-
-    if (retCode == ERROR_SUCCESS)
-    {
-      if (lpType == REG_SZ)
-      {
-        auto text = (char *)buffer;
-        auto obj = CreateEntry(achValue, "REG_SZ", text);
-        Nan::Set(a, i, obj);
-      }
-      else if (lpType == REG_EXPAND_SZ)
-      {
-        auto text = (char *)buffer;
-        auto obj = CreateEntry(achValue, "REG_EXPAND_SZ", text);
-        Nan::Set(a, i, obj);
-      }
-      else if (lpType == REG_DWORD)
-      {
-        // NOTE:
-        // at this point the value in buffer looks like this: '\x124242'
-        // i haven't figured out an easy way to parse this, so I'm going to make
-        // a second call because I know I can get the value out in the correct
-        // format in this way and avoid messing with strings
-        unsigned long size = 1024;
-
-        LONG nError = RegQueryValueEx(hCurrentKey, achValue, NULL, &lpType, (LPBYTE)&cbData, &size);
-        if (ERROR_SUCCESS == nError)
-        {
-          Nan::Set(a, i, CreateEntry(achValue, "REG_DWORD", cbData));
-        }
-      }
-    }
-    else if (retCode != ERROR_NO_MORE_ITEMS)
-    {
-      char* errorMessage = NULL;
-      sprintf(errorMessage, "RegEnumValue returned an error code: '%d'", retCode);
-      Nan::ThrowTypeError(errorMessage);
-    }
-  }
-
-  RegCloseKey(hCurrentKey);
 }
 
 void Init(v8::Handle<v8::Object> exports) {
