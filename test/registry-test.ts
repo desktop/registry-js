80fbe5fd4d592a8c199cd9741ce3170c99896c8e
HashimotoYT <hashimoto.stream@gmail.com>
2019-01-31 22:43:15 +0200

    Port tests from mocha/chai to jest

--- test/registry-test.ts
+++ test/registry-test.ts
@@ -1,4 +1,3 @@
-import { expect } from "chai";
 import { enumerateValues, enumerateKeys, HKEY } from "../lib/index";
 
 if (process.platform === "win32") {
@@ -10,12 +9,12 @@ if (process.platform === "win32") {
       );
 
       const programFilesDir = values.find(v => v.name == "ProgramFilesDir");
-      expect(programFilesDir!.type).equals("REG_SZ");
-      expect(programFilesDir!.data).contains(":\\Program Files");
+      expect(programFilesDir!.type).toBe("REG_SZ");
+      expect(programFilesDir!.data).toContain(":\\Program Files");
 
       const programFilesPath = values.find(v => v.name == "ProgramFilesPath");
-      expect(programFilesPath!.type).equals("REG_EXPAND_SZ");
-      expect(programFilesPath!.data).equals("%ProgramFiles%");
+      expect(programFilesPath!.type).toBe("REG_EXPAND_SZ");
+      expect(programFilesPath!.data).toBe("%ProgramFiles%");
     });
 
     it("can read numbers from the registry", () => {
@@ -25,8 +24,8 @@ if (process.platform === "win32") {
       );
 
       const serviceTimeout = values.find(v => v.name == "ServiceTimeout");
-      expect(serviceTimeout!.type).equals("REG_DWORD");
-      expect(serviceTimeout!.data).equals(60000);
+      expect(serviceTimeout!.type).toBe("REG_DWORD");
+      expect(serviceTimeout!.data).toBe(60000);
     });
 
     it("can read values from HKCU", () => {
@@ -36,29 +35,29 @@ if (process.platform === "win32") {
       );
 
       const composition = values.find(v => v.name == "Composition");
-      expect(composition!.type).equals("REG_DWORD");
-      expect(composition!.data).equals(1);
+      expect(composition!.type).toBe("REG_DWORD");
+      expect(composition!.data).toBe(1);
     });
 
     it("returns empty array when key is missing", () => {
       const values = enumerateValues(HKEY.HKEY_LOCAL_MACHINE, "blahblahblah");
 
-      expect(values).is.empty;
+      expect(values).toEqual([]);
     });
   });
   describe("enumerateKeys", () => {
     it("can enumerate key names from the registry (No subkey)", () => {
       const values = enumerateKeys(HKEY.HKEY_LOCAL_MACHINE, null);
 
-      expect(values.indexOf("HARDWARE")).gte(0);
-      expect(values.indexOf("SOFTWARE")).gte(0);
-      expect(values.indexOf("SYSTEM")).gte(0);
+      expect(values.indexOf("HARDWARE")).toBeGreaterThanOrEqual(0);
+      expect(values.indexOf("SOFTWARE")).toBeGreaterThanOrEqual(0);
+      expect(values.indexOf("SYSTEM")).toBeGreaterThanOrEqual(0);
     });
     it("can enumerate key names from the registry", () => {
       const values = enumerateKeys(HKEY.HKEY_LOCAL_MACHINE, "SOFTWARE");
 
-      expect(values.indexOf("Classes")).gte(0);
-      expect(values.indexOf("Microsoft")).gte(0);
+      expect(values.indexOf("Classes")).toBeGreaterThanOrEqual(0);
+      expect(values.indexOf("Microsoft")).toBeGreaterThanOrEqual(0);
     });
   });
 }
